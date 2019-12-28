df3a311853c2916e9f6117a0705788b7473dc49b
Ruben Kaiser <Ruben@C02RJ36JG8WP.local>
2019-12-28 14:33:57 +0100

    fixed menu

--- parcel/src/index.js
+++ parcel/src/index.js
@@ -1,1 +1,18 @@
 import './scss/style.scss';
+
+const navigation = document.getElementById('navigation');
+
+const toggleMenu = () => {
+  console.log('toggle');
+  navigation.classList.toggle('visible');
+}
+
+const bodyClick = () => {
+  console.log('bodyclick');
+  if (navigation.classList.contains('visible')) {
+    navigation.classList.toggle('visible');
+  }
+}
+
+document.body.addEventListener('click', bodyClick, true);
+document.getElementById('menuBtn').addEventListener('click', toggleMenu);
